5289f7f6794bd40cb4be505fb6fcfa2534e659ea
mendalas <1309745657@qq.com>
2019-04-25 14:48:09 +0800

    写着玩

--- http/httpRequest.cpp
+++ http/httpRequest.cpp
@@ -1,7 +1,6 @@
 #include "httpRequest.h"
 #include <sstream>
 
-
 httpRequest::handleRequest(std::string& request_string)
 {
     int beforeCursor=0;
@@ -9,6 +8,23 @@ httpRequest::handleRequest(std::string& request_string)
 
     afterCursor=request_string.find_first_of(" ",beforeCursor);    
     string httpMethod=request_string.substr(beforeCursor,afterCursor-beforeCursor);
+
+    if(httpMethod=="GET")
+        _Method=GET;
+    else 
+        if(httpMethod=="PUT")
+        _Method=PUT;
+    else 
+        if(httpMethod=="HEAD")
+        _Method=HEAD;
+    else 
+        if(httpMethod=="POST")
+        _Method=POST;
+    else 
+        _Method=NOT_IMPLEMENTED;
+
     
-    if()
+
+
+
 }
